76b387123676aa59f8be7b604cff2919a2691e50
Changmin Kim <Changmin043@gmail.com>
2017-11-13 23:55:52 +0900

    반영되지 않은 풀이 소스 반영

--- BaekJoon/BJ10799.java
+++ BaekJoon/BJ10799.java
@@ -1,7 +1,40 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-/**
- * Created by changmin on 2017. 11. 13..
- */
 public class BJ10799 {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        String input = scanner.nextLine();
+        int output = calPiece(input);
+        System.out.println(output);
+    }
+
+    private static int calPiece(String input) {
+        int output = 0;
+        Stack<Character> stack = new Stack<>();
+        char[] tokens = input.toCharArray();
+
+        for(int i = 0; i < tokens.length; ++i){
+            if(stack.empty()) {
+                stack.push(tokens[i]);
+            }
+            else if(tokens[i-1] == '('){
+                if('(' == tokens[i]){
+                    stack.push(tokens[i]);
+                }else {
+                    stack.pop();
+                    output += stack.size();
+                }
+            }
+            else if (tokens[i-1] == ')'){
+                if('(' == tokens[i]){
+                    stack.push(tokens[i]);
+                }else{
+                    stack.pop();
+                    output+= 1;
+                }
+            }
+        }
+        return output;
+    }
 }
